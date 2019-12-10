9cabc3668274eb55db329c45e4145ffdaa617185
Sylwia Vargas <45401242+sylwiavargas@users.noreply.github.com>
2019-12-10 16:33:24 -0500

    Update piglatinizer.rb

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,19 +6,19 @@ class PigLatinizer
 
   private
 
-  def consonant?(char)
-    !char.match(/[aAeEiIoOuU]/)
+  def vowel?(char)
+    char.match(/[aAeEiIoOuU]/)
   end
 
   def piglatinize_word(word)
     # word starts with vowel
-    if !consonant?(word[0])
+    if vowel?(word[0])
       word = word + "w"
     # word starts with 3 consonants
-    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+    elsif !vowel?(word[0]) && !vowel?(word[1]) && vowel?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
     # word starts with 2 consonants
-    elsif consonant?(word[0]) && consonant?(word[1])
+    elsif vowel?(word[0]) && !vowel?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
     # word starts with 1 consonant
     else
